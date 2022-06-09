944906b3c1760e3c04b22aab023a882759e673f4
sohal <tosohal@gmail.com>
2022-06-09 19:49:36 +0200

    Math library added in linker

--- programs/consequtive/main.c
+++ programs/consequtive/main.c
@@ -12,5 +12,6 @@ int main(int argc, char *argv[])
     const uint8_t bits = 1;
     uint8_t c=ShiftLeftByBits(input,bits);
     printf("The output is %u for %u shifted left by %u bits\n", c, input, bits);
+    printf("The output is %u for %u * (2^%u)\n", (uint32_t)(input * pow(2,bits)), input, bits);
 
 }
