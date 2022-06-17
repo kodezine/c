187601905ea0c550120509a813c3bf2682eb9de0
Adittya <adittyamittal.baroda@gmail.com>
2022-06-17 11:02:07 +0530

    testing

--- programs/consequtive/test/ut_consequtive.c
+++ programs/consequtive/test/ut_consequtive.c
@@ -5,4 +5,6 @@ void test_consequtive_areNumbersConsequtive(void)
 {
     bool retVal = consequtive_areNumbersConsequtive('a','b');
     TEST_ASSERT_EQUAL(true, retVal);
+    retVal = consequtive_areNumbersConsequtive('x','z');
+    TEST_ASSERT_EQUAL(false, retVal);
 }
