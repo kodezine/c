6e4fe13d15c637bbe1afef8ec2c3be7ed1d15547
sohal <tosohal@gmail.com>
2022-06-23 12:16:44 +0200

    Added some test case for overflow

--- programs/adittya/adittya.c
+++ programs/adittya/adittya.c
@@ -1,16 +1,20 @@
-#include<stdio.h>
+#include<stdbool.h>
+#include<stddef.h>
 #include"adittya.h"
 
 int32_t adittya_one(int32_t *a , int32_t *b)
 {
-    int32_t returnValue=0;
-    if(a==NULL || b==NULL)
-    {
-        ;       
-    }
-    else
-    {
-        returnValue= *a +*b;
-    }
+    int32_t returnValue = 0;
+    do{
+        if(a == NULL)
+        {
+            break;
+        }
+        if(b == NULL)
+        {
+            break;
+        }
+        returnValue = (*a) + (*b);
+    }while(false);
     return returnValue;
 }
--- programs/adittya/test/ut_adittya.c
+++ programs/adittya/test/ut_adittya.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include"ut_adittya.h"
 #include"adittya.h"
 
@@ -29,5 +30,11 @@ void test_adittya_adittya_one_additionNullPtr(void)
     TEST_ASSERT_EQUAL(30, retVal);
 }
 
-
+void test_adittya_adittya_one_CheckUpperLimit(void)
+{
+    int32_t a = INT32_MAX;
+    int32_t b = 1;
+    int32_t retVal = adittya_one(&a, &b);
+    TEST_ASSERT_EQUAL_INT64((int64_t)INT32_MAX+1, retVal);
+}
 
